5071770f7f3e6e43caf2414d2ae6e493784c214f
LuisPa Garcia <diganluispa@gmail.com>
2021-04-19 13:56:39 -0600

    fix: Fixed vulnerability

--- src/set.ts
+++ src/set.ts
@@ -1,53 +1,64 @@
+"use strict";
+
+const disallowedKeys = new Set(["__proto__", "prototype", "constructor"]);
+
+const isValidPath = (pathSegments) =>
+  !pathSegments.some((segment) => disallowedKeys.has(segment));
+
 /**
  * @param {object} object base object to inject new prop
  * @param {string} path path to set the object
  * @param {any} value value to set
  */
 function propSetter(object: Object, path: string, value: any): Object {
-	function pathSeg(path: string): string[] {
-		const pathAr: string[] = path.split('.');
-		const parts: string[] = [];
+  function pathSeg(path: string): string[] {
+    const pathAr: string[] = path.split(".");
+    const parts: string[] = [];
 
-		for (let i: number = 0; i < pathAr.length; i++) {
-			let p = pathAr[i];
+    for (let i: number = 0; i < pathAr.length; i++) {
+      let p = pathAr[i];
 
-			while (p[p.length - 1] === '\\' && pathAr[i + 1] !== undefined) {
-				p = p.slice(0, -1) + '.';
-				p += pathAr[++i];
-			}
+      while (p[p.length - 1] === "\\" && pathAr[i + 1] !== undefined) {
+        p = p.slice(0, -1) + ".";
+        p += pathAr[++i];
+      }
 
-			parts.push(p);
-		}
+      parts.push(p);
+    }
 
-		return parts;
-	}
-	function isObject(value): boolean {
-		const type: string = typeof value;
-		return value !== null && (type === 'object' || type === 'function');
-	}
+    return parts;
+  }
+  function isObject(value): boolean {
+    const type: string = typeof value;
+    return value !== null && (type === "object" || type === "function");
+  }
 
-	if (!isObject(object) || typeof path !== 'string') {
-		return JSON.parse(JSON.stringify(object));
-	}
+  if (!isObject(object) || typeof path !== "string") {
+    return JSON.parse(JSON.stringify(object));
+  }
 
-	const ret: Object = object;
-	const pathAr: string[] = pathSeg(path);
+  const ret: Object = object;
+  const pathAr: string[] = pathSeg(path);
 
-	for (let i: number = 0; i < pathAr.length; i++) {
-		const p = pathAr[i];
+  for (let i: number = 0; i < pathAr.length; i++) {
+    const p = pathAr[i];
 
-		if (!isObject(object[p])) {
-			object[p] = {};
-		}
+    if (!isValidPath(p)) {
+      return [];
+    }
 
-		if (i === pathAr.length - 1) {
-			object[p] = value;
-		}
+    if (!isObject(object[p])) {
+      object[p] = {};
+    }
 
-		object = object[p];
-	}
+    if (i === pathAr.length - 1) {
+      object[p] = value;
+    }
 
-	return JSON.parse(JSON.stringify(ret));
+    object = object[p];
+  }
+
+  return JSON.parse(JSON.stringify(ret));
 }
 
-export{ propSetter as set };
+export { propSetter as set };
